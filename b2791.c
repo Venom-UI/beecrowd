329be6ddad9f780042de9359ebd6b19e97781fb2
Ingrid Jeanine Goncalves de Oiveira <125323073+ingridJeanine@users.noreply.github.com>
2023-04-11 13:46:57 -0300

    Add files via upload

---
+++ b2791.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int c1, c2, c3, c4;
+    scanf("%d %d %d %d", &c1, &c2, &c3, &c4);
+    if (c1 == 1)
+    printf("1\n");
+    if (c2 == 1)
+    printf("2\n");
+    if (c3 == 1)
+    printf("3\n");
+    if (c4 == 1)
+    printf("4\n");
+    return 0;
+}
